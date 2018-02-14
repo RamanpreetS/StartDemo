ed0c513cf996f3c7dff8686b8cc78323025cd8ba
Ramanpreet Singh <ramanpreet.singh@mobileprogramming.net>
2018-02-14 10:51:20 -0800

    add new changes

--- SelfDemoGit/SelfDemoGit/ViewController.swift
+++ SelfDemoGit/SelfDemoGit/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         //last testing swap
         //AnkushTesting
         
-        // trunck testing for development
+        // lap trunck testing for development
         // Do any additional setup after loading the view, typically from a nib.
     }
 
